1bfd9470c28200788fd0b1459f0afc87295ac08f
carineong <58885856+carineong@users.noreply.github.com>
2020-07-11 22:11:21 +0800

    Add files via upload

---
+++ D1/star.py
@@ -0,0 +1,18 @@
+# star.py
+# error!!
+
+import turtle
+
+size = int(input("Please input size:(20~200)"))
+
+t = turtle.Turtle()
+t.color('red')
+t.pensize(3)
+
+for i in range(5):
+  t.forward(size)
+  t.right(144)
+
+t.hideturtle()
+turtle.done()
+
